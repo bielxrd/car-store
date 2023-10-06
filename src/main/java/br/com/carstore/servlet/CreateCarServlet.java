aa7ea62ef39db9a93c5a8d8ab3fd2da535d746f3
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-06 15:07:36 -0300

    Atualizacao do metodo doPost, puxando funcao createCar para enviar para o banco de dados o cadastro do carro.

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDAO;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +17,9 @@ public class CreateCarServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String carName = req.getParameter("car-name");
 
-        System.out.println(carName);
+        Car car = new Car();
+        car.setName(carName);
+        new CarDAO().createCar(car);
 
         req.getRequestDispatcher("index.html").forward(req, resp);
     }
