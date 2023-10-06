8a8c09fdb23a34f0d6b10744d270390c73f14cc0
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-06 14:30:48 -0300

    criado arquivo java CreateCarServlet, com sobreposicao do metodo doPost, para requisicao e resposta do que vem do webapp.

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,21 @@
+package br.com.carstore.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String carName = req.getParameter("car-name");
+
+        System.out.println(carName);
+
+        req.getRequestDispatcher("index.html").forward(req, resp);
+    }
+}
