bf19d8c43f8e309217718922d7d3abeb0a5f96e1
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-07 05:23:24 -0300

    ajuste da funcao para redirecionar para pagina dos carros(dashboard.jsp)

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,6 +1,6 @@
 package br.com.carstore.servlet;
 
-import br.com.carstore.dao.CarDAO;
+import br.com.carstore.dao.CarDao;
 import br.com.carstore.model.Car;
 
 import javax.servlet.ServletException;
@@ -9,17 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
         String carName = req.getParameter("car-name");
 
         Car car = new Car(carName);
-        new CarDAO().createCar(car);
 
-        req.getRequestDispatcher("index.html").forward(req, resp);
+        new CarDao().createCar(car);
+
+        resp.sendRedirect("/find-all-cars");
+
     }
+
 }
