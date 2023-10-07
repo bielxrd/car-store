6b4968694bbb6948ac59a3de4dd4083bad32d2c1
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-07 03:03:20 -0300

    construtor e remocao do metodo setName

--- src/main/java/br/com/carstore/model/Car.java
+++ src/main/java/br/com/carstore/model/Car.java
@@ -3,11 +3,12 @@ package br.com.carstore.model;
 public class Car {
     private String name;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
