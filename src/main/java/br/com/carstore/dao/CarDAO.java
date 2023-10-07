3c643d47f04ffa70ca3c9f46add0f05aaf7a6081
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-07 03:02:50 -0300

    metodo findAllCars(select * from)

--- src/main/java/br/com/carstore/dao/CarDAO.java
+++ src/main/java/br/com/carstore/dao/CarDAO.java
@@ -5,6 +5,10 @@ import br.com.carstore.model.Car;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CarDAO {
 
@@ -24,6 +28,36 @@ public class CarDAO {
             connection.close();
         } catch (Exception e) {
             System.out.println("error in connection");
+        }
+    }
+
+    public List<Car> findAllCars() {
+        String SQL = "SELECT * FROM CAR";
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+            System.out.println("sucess in connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Car> cars = new ArrayList<>();
+            while (resultSet.next()) {
+
+                Car car = new Car(resultSet.getString("name"));
+
+                cars.add(car);
+            }
+
+            System.out.println("sucess in select * car");
+
+            connection.close();
+
+            return cars;
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+            return Collections.emptyList();
         }
     }
 }
