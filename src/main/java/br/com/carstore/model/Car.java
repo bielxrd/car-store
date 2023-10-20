ef767f9342f26c69fb3efcb972c9de01e4993930
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-20 01:26:34 -0300

    Adicionado construtor novo e Getter do ID

--- src/main/java/br/com/carstore/model/Car.java
+++ src/main/java/br/com/carstore/model/Car.java
@@ -1,14 +1,25 @@
 package br.com.carstore.model;
 
 public class Car {
+
+    private String id;
     private String name;
+
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public Car(String name, String id) {
+        this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getId() {
+        return id;
+    }
 }
